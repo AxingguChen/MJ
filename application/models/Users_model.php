317f9b2a5c4987ed3793fcd56c4f5a26e06669af
jackie <axinggu@qq.com>
2015-10-11 15:41:04 +0300

    login

---
+++ application/models/Users_model.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jackie
+ * Date: 10/11/2015
+ * Time: 3:12 PM
+ * model for users table
+ */
+
+
+class Users_model extends CI_Model {
+
+
+
+    /**
+     * select * from users
+     *
+     * @access public
+     * @return all users
+     */
+    function get_all() {
+        // $this -> db -> select('*');
+        $this->db->from ( 'users' );
+        // $this -> db -> limit(1);
+
+        $query = $this->db->get ();
+        return $query->result ();
+    }
+
+
+    /**
+     * provide login input of email and password
+     * return user info if match
+     *
+     * @access public
+     * @param string $email
+     * @param stting $password
+     * @return login user records
+     */
+    function login($email, $password) {
+        $this->db->select ( 'users_id, email, password' );
+        $this->db->from ( 'users' );
+        $this->db->where ( 'email', $email );
+        $this->db->where ( 'password', sha1 ( $password ) );
+        // $this -> db -> limit(1);
+
+        $query = $this->db->get ();
+
+        if ($query->num_rows () == 1) {
+            return $query->result ();
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * input from form
+     * intser the data to users(table)
+     *
+     * @access public
+     * @return null
+     */
+    function register() {
+        $data = array (
+            'email' => $this->input->post ( 'email' ),
+            'username' => $this->input->post ( 'username' ),
+            'users_groups_id' => 12,
+            'password' => sha1 ( $this->input->post ( 'password' ) )
+        );
+        $this->db->insert ( 'users', $data );
+    }
+
+    /**
+     * select * from users where email = $email
+     * return match user
+     *
+     * @access public
+     * @param string $email
+     * @return match user
+     */
+    function getByEmail($email) {
+        // $this -> db -> select('id, email');
+        $this->db->from ( 'users' );
+        $this->db->where ( 'email', $email );
+        // $this -> db -> limit(1);
+
+        $query = $this->db->get ();
+
+        if ($query->num_rows () == 1) {
+            return $query->result ();
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * select * from users where id = $id
+     * return match user
+     *
+     * @access public
+     * @param string $id
+     * @return match user
+     */
+    function getById($id) {
+        // $this -> db -> select('id, email');
+        $this->db->from ( 'users' );
+        $this->db->where ( 'users_id', $id );
+        // $this -> db -> limit(1);
+
+        $query = $this->db->get ();
+
+        if ($query->num_rows () == 1) {
+            return $query->result ();
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * input from form
+     * update the data to users(table)
+     *
+     * @access public
+     * @return null
+     */
+    function update_profile($id, $data) {
+
+        $this->db->where('users_id', $id);
+        $this->db->update('users', $data);
+
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === FALSE)
+        {
+            //
+            return -1;
+        }
+        else
+        {
+            return $this->db->affected_rows();
+        }
+
+    }
+}
+
---
+++ application/views/login_view.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jackie
+ * Date: 10/11/2015
+ * Time: 3:13 PM
+ */
+?>
+
+<body>
+<!-- contact form -->
+<div class="text-center wow fadeInDown  text-center animated"
+     data-wow-duration="500ms" data-wow-delay="300ms">
+    <div class="contact-form">
+
+        <div class="input-group row firstname-lastname">
+
+            <?php echo form_open("verification/verify"); ?>
+            <div class="input-text-field col-md-1">Email:</div>
+            <div class="input-field col-md-4">
+                <input type="text" name="email" id="email"
+                       placeholder="email address" class="form-control">
+            </div>
+            <div class="input-text-field col-md-1">Passowrd:</div>
+            <div class="input-field col-md-4">
+                <input type="password" name="password" id="password"
+                       placeholder="password" class="form-control">
+            </div>
+            <div class="input-group">
+                <input type="submit" id="form-submit" class="pull-right"
+                       value="Login here">
+            </div>
+            <?php echo form_close(); ?>
+        </div>
+
+
+
+    </div>
+</div>
+<!-- end contact form -->
+
+</body>
